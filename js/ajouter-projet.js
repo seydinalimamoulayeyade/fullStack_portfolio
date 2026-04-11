0ca7881b0cdb075d809583d17e70ae3e2bf62279
Seydina Limamou Laye <seydinalimamoulayeyade@gmail.com>
2026-04-11 11:55:57 +0000

    feat: ajout de la gestion des projets avec validation de formulaire et stockage local

--- js/ajouter-projet.js
+++ js/ajouter-projet.js
@@ -1,1 +1,8 @@
 import "./main.js";
+import { initFormAdmin } from "./modules/form-admin.js";
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initFormAdmin);
+} else {
+  initFormAdmin();
+}

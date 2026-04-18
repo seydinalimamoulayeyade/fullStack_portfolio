dd37cb1be4ea079e306a1865cf101c8967a4bd19
Seydina Limamou Laye Yade <109959995+seydinalimamoulayeyade@users.noreply.github.com>
2026-04-18 17:09:31 +0100

    refactor: retirer Supabase et revenir à une initialisation locale de l’admin

--- js/ajouter-projet.js
+++ js/ajouter-projet.js
@@ -1,14 +1,11 @@
 import "./main.js";
 import { initFormAdmin } from "./modules/form-admin.js";
 import { initManageProjects } from "./modules/manage-projects.js";
-import { requireAdmin } from "./lib/admin-guard.js";
 
 async function init() {
   try {
-    await requireAdmin();
-
-    initFormAdmin();
-    initManageProjects();
+    await initFormAdmin();
+    await initManageProjects();
   } catch (error) {
     console.error(error);
   }

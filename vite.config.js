e9bdc86761b581d3f86c1b7b386ef7528fcf40e5
Seydina Limamou Laye <seydinalimamoulayeyade@gmail.com>
2026-04-23 18:54:07 +0000

    chore: update Vite configuration for multi-page setup and enhance GitHub Actions workflow steps

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/fullStack_portfolio/',
+  base: "/fullStack_portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "lister-projets.html"),
+        addProject: resolve(__dirname, "ajouter-projet.html"),
+        projectDetail: resolve(__dirname, "detailler-projet.html"),
+      },
+    },
+  },
 });
